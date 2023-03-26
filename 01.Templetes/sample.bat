0b74cdac346750dbca8e61a67806403ea1d9a20c
Yusuke <aqua.tsu.pooh@gmail.com>
2023-03-26 22:17:59 +0900

    up

--- 01.Templetes/sample.bat
+++ 01.Templetes/sample.bat
@@ -1,16 +1,16 @@
 @ECHO OFF
 :: +--------------------------------------------------
-:: | Create/Author  | 20xx.xx.xx / Name 
+:: | Create/Author  | 2023.03.26 / Yusuke 
 :: | Update/Author  | 20yy.yy.yy / Name
 :: | Version        | 1.0
-:: | Overview       |
+:: | Overview       | 
 :: +--------------------------------------------------
 
 :: Environment
 SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 
 SET NOWDATE=%date:~0,4%%date:~5,2%%date:~8,2%
-SET LOGFLODER=C:\logs\
+SET LOGFOLDER=C:\logs\
 SET LOGFILENAME=Process_%NOWDATE%.log
 
 :: CurrentDir
@@ -18,15 +18,29 @@ PUSHD %~DP0
 
 :: MainMethod
 :Main
+  CALL :Init
 
+:: MainWork
+
+
+:: TerminateWork
   GOTO :Terminate
+
+
+:: Initial
+:Init
+  IF EXIST "%LOGFOLDER%" (
+  ) ELSE (
+    MKDIR %LOGFOLDER%
+  )
+  EXIT /b
 
 :: SubMethod
 :Sub
-
 
 :: Terminate
 :Terminate
   POPD
   ENDLOCAL
+  pause
   EXIT /b
